51d0b279d536d67f19acd59097b1c3a0a50ea9c3
@tufla <henry@justontract.me>
2013-06-06 10:00:29 -0500

    Adding slides

--- demo/urls.py
+++ demo/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
 from app.api import CompanyResource, StockResource
@@ -7,5 +8,11 @@ v1_api.register(CompanyResource())
 v1_api.register(StockResource())
 
 urlpatterns = patterns('',
+    url(r'^$', 'app.views.home'),
     url(r'^api/', include(v1_api.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
