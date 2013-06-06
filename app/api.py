4a4f85b62324eb4c490c1e4ae3ec2365fa13b679
@tufla <henry@justontract.me>
2013-06-06 12:01:06 -0500

    Adding examples
    Adding examples
    
    Examples shown

--- app/api.py
+++ app/api.py
@@ -1,19 +1,84 @@
 from app.models import Company, Stock
 from tastypie import fields
 from tastypie.authorization import Authorization
-from tastypie.exceptions import BadRequest
-from tastypie.paginator import Paginator
+# from tastypie.exceptions import BadRequest
+# from tastypie.paginator import Paginator
 from tastypie.resources import ModelResource, ALL_WITH_RELATIONS
 
+#------------ Basic resource
+# class CompanyResource(ModelResource):
 
+#     class Meta:
+#         queryset = Company.objects.all()
+
+
+#------------ Allowing GET/POST/PUT/DELETE
+# class CompanyResource(ModelResource):
+
+#     class Meta:
+#         queryset = Company.objects.all()
+#         authorization = Authorization()
+
+
+#------------ Adding more resources
+# class StockResource(ModelResource):
+#     company = fields.ToOneField('app.api.CompanyResource', 'company')
+
+#     class Meta:
+#         queryset = Stock.objects.all()
+
+
+#------------ Excluding fields
+# class StockResource(ModelResource):
+#     company = fields.ToOneField('app.api.CompanyResource', 'company')
+
+#     class Meta:
+#         queryset = Stock.objects.all()
+#         resource_name = 'stock'
+#         excludes = ['Open', 'High', 'Low']
+
+#------------ Showing exclusive fields
+# class StockResource(ModelResource):
+#     company = fields.ToOneField('app.api.CompanyResource', 'company')
+
+#     class Meta:
+#         queryset = Stock.objects.all()
+#         resource_name = 'stock'
+#         fields = ['company', 'Date', 'Volume']
+
+
+#------------ Limiting access
+# class StockResource(ModelResource):
+#     company = fields.ToOneField('app.api.CompanyResource', 'company')
+
+#     class Meta:
+#         queryset = Stock.objects.all()
+#         resource_name = 'stock'
+#         fields = ['company', 'Date', 'Volume']
+#         allowed_methods = ['get']
+
+#------------ Filtering & ordering
 class CompanyResource(ModelResource):
 
     class Meta:
         queryset = Company.objects.all()
-
+        authorization = Authorization()
+        filtering = {
+            'name': ['exact', 'startswith']
+        }
+        ordering = {
+            'name': ['name']
+        }
 
 class StockResource(ModelResource):
     company = fields.ToOneField('app.api.CompanyResource', 'company')
 
     class Meta:
         queryset = Stock.objects.all()
+        resource_name = 'stock'
+        fields = ['company', 'Date', 'Volume']
+        allowed_methods = ['get']
+        filtering = {
+            'company': ALL_WITH_RELATIONS,
+            'Date': ['exact', 'lt', 'lte', 'gte', 'gt'],
+        }
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=200, primary_key=True)
-    created = models.DateField()
+    created = models.DateField(auto_now_add=True)
 
 
 class Stock(models.Model):
